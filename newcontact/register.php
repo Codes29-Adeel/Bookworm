ac991e834ab0ad0a2e724eed01608ef79c904b3f
Codes29-Adeel <specialcodes29@gmail.com>
2025-09-10 22:07:04 +0500

    Log pro fix

--- newcontact/register.php
+++ newcontact/register.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "newcontact");
+$conn = new mysqli("localhost", "root", "","newcontact");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = $_POST["name"];
